c93dc3cbac7bc497388bbca69bef87706ae961bd
BurakSahin2060 <burak.sahin@student.htldornbirn.at>
2024-09-20 11:28:18 +0200

    First Assignment Completed

--- 1_HelloSWP_CS/Program.cs
+++ 1_HelloSWP_CS/Program.cs
@@ -1,3 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Bitte geben Sie eine Zeichenkette ein: ");
+// Console.WriteLine("Bitte geben Sie eine Zeichenkette ein: ");
 
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input;
+
+        while (true)
+        {
+            Console.WriteLine("Bitte eine Eingabe tätigen (oder 'Beenden' zum Beenden):");
+
+            input = Console.ReadLine();
+
+            if (input.Equals("Beenden", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Programm wird beendet.");
+                break;
+            }
+
+            Console.WriteLine("Eingegebener Text: " + input);
+        }
+    }
+}
