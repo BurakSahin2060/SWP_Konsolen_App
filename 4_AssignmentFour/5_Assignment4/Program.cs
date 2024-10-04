add8b2a4d0de9a5cb1dc547f38067bb0195fd008
BurakSahin2060 <burak.sahin@student.htldornbirn.at>
2024-10-04 12:19:09 +0200

    Renamed Folder(s)

---
+++ 4_AssignmentFour/5_Assignment4/Program.cs
@@ -0,0 +1,6 @@
+﻿//Erweitern Sie ihre Konsolenanwendung in der Art, dass nach dem Einlesen der Natürlichen Zahl eine Abfrage erscheint, in der Sie eine Mathematische Operation auswählen können... Dies kann zum Beispiel so ausschauen:
+//Geben Sie eine Zahl zwischen 1 und 3 ein um eine Mathematische Operation auszuwählen:
+//1) Quadrat
+//2) Wurzel
+//3) Fakultät
+
