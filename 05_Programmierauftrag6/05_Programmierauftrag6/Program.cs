d649f01979d66519db1aec052bfceaf456a654c7
BurakSahin2060 <burak.sahin@student.htldornbirn.at>
2024-10-18 11:39:41 +0200

    Programmierauftrag 6 | OPTIMIERT

--- 05_Programmierauftrag6/05_Programmierauftrag6/Program.cs
+++ 05_Programmierauftrag6/05_Programmierauftrag6/Program.cs
@@ -11,8 +11,8 @@
 // Je nach Auswahl soll im Anschluss entweder eine oder zwei Ganzzahlen eingelesen 
 // und die entsprechende mathematische Operation ausgeführt werden.
 
-
 using System;
+
 class Program
 {
     static void Main()
@@ -31,59 +31,65 @@ class Program
             Console.WriteLine("Geben Sie 'Beenden' ein, um das Programm zu beenden.");
 
             string operationInput = Console.ReadLine();
-
             if (operationInput.Equals("Beenden", StringComparison.OrdinalIgnoreCase))
             {
                 Console.WriteLine("Programm wird beendet.");
                 break;
             }
-
+            int zahl1, zahl2;
             switch (operationInput)
             {
                 case "1": // Multiplizieren
+                    (zahl1, zahl2) = ReadInTwoNumbers();
+                    Console.WriteLine($"{zahl1} * {zahl2} = {zahl1 * zahl2}");
+                    break;
+
                 case "2": // Dividieren
-                case "3": // Addieren
-                case "4": // Potenzieren
-                    Console.WriteLine("Bitte geben Sie die erste Zahl ein:");
-                    int zahl1 = int.Parse(Console.ReadLine());
-
-                    Console.WriteLine("Bitte geben Sie die zweite Zahl ein:");
-                    int zahl2 = int.Parse(Console.ReadLine());
-
-                    switch (operationInput)
+                    (zahl1, zahl2) = ReadInTwoNumbers();
+                    if (zahl2 != 0)
                     {
-                        case "1":
-                            Console.WriteLine($"{zahl1} * {zahl2} = {zahl1 * zahl2}");
-                            break;
-                        case "2":
-                            if (zahl2 != 0)
-                            {
-                                Console.WriteLine($"{zahl1} / {zahl2} = {zahl1 / (double)zahl2}");
-                            }
-                            else
-                            {
-                                Console.WriteLine("Division durch Null ist nicht erlaubt.");
-                            }
-                            break;
-                        case "3":
-                            Console.WriteLine($"{zahl1} + {zahl2} = {zahl1 + zahl2}");
-                            break;
-                        case "4":
-                            Console.WriteLine($"{zahl1} ^ {zahl2} = {Math.Pow(zahl1, zahl2)}");
-                            break;
+                        Console.WriteLine($"{zahl1} / {zahl2} = {zahl1 / (double)zahl2}");
                     }
+                    else
+                    {
+                        Console.WriteLine("Division durch Null ist nicht erlaubt.");
+                    }
+                    break;
+
+                case "3": // Addieren
+                    (zahl1, zahl2) = ReadInTwoNumbers();
+                    Console.WriteLine($"{zahl1} + {zahl2} = {zahl1 + zahl2}");
+                    break;
+
+                case "4": // Potenzieren
+                    (zahl1, zahl2) = ReadInTwoNumbers();
+                    Console.WriteLine($"{zahl1} ^ {zahl2} = {Math.Pow(zahl1, zahl2)}");
                     break;
 
                 case "5": // Wurzelziehen
                     Console.WriteLine("Bitte geben Sie eine Zahl ein:");
                     int wurzelZahl = int.Parse(Console.ReadLine());
-                    Console.WriteLine($"Die Wurzel von {wurzelZahl} ist {Math.Sqrt(wurzelZahl)}");
+                    if (wurzelZahl >= 0)
+                    {
+                        Console.WriteLine($"Die Wurzel von {wurzelZahl} ist {Math.Sqrt(wurzelZahl)}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wurzelziehen für negative Zahlen ist nicht definiert.");
+                    }
                     break;
 
                 case "6": // Fakultät
                     Console.WriteLine("Bitte geben Sie eine natürliche Zahl ein:");
                     int fakultaetZahl = int.Parse(Console.ReadLine());
-                    Console.WriteLine($"Die Fakultät von {fakultaetZahl} ist {BerechneFakultaet(fakultaetZahl)}");
+                    if (fakultaetZahl >= 0)
+                    {
+                        Console.WriteLine($"Die Fakultät von {fakultaetZahl} ist {BerechneFakultaet(fakultaetZahl)}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Fakultät ist nur für nicht-negative Zahlen definiert.");
+                    }
                     break;
 
                 case "7": // Invertieren
@@ -104,6 +110,16 @@ class Program
                     break;
             }
         }
+    }
+
+    static (int, int) ReadInTwoNumbers()
+    {
+        Console.WriteLine("Bitte geben Sie die erste Zahl ein:");
+        int zahl1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Bitte geben Sie die zweite Zahl ein:");
+        int zahl2 = int.Parse(Console.ReadLine());
+        return (zahl1, zahl2);
     }
 
     static long BerechneFakultaet(int zahl)
