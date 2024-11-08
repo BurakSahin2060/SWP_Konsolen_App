ee35917f4af8e18fef72f14ba701945bcea4d7d6
BurakSahin2060 <burak.sahin@student.htldornbirn.at>
2024-11-08 11:51:05 +0100

    Age of Cat | DONE

---
+++ 07_Objektorientierung/07_Objektorientierung/07_Objektorientierung/Cat.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace _07_Objektorientierung
+{
+    class Cat
+    {
+        private string _Color;
+        private DateTime _BirthDate;
+
+        public string Color
+        {
+            get { return _Color; }
+            set
+            {
+                if (_Color == value) return;
+                _Color = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                var today = DateTime.Today;
+                int age = today.Year - _BirthDate.Year;
+                if (_BirthDate.Date > today.AddYears(-age)) age--;
+                return age;
+            }
+        }
+
+        public Cat(DateTime birthdate)
+        {
+            _BirthDate = birthdate;
+        }
+    }
+}
---
+++ 07_Objektorientierung/07_Objektorientierung/07_Objektorientierung/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _07_Objektorientierung;
+
+Console.WriteLine("Hello, World!");
+
+// Instanz der Klasse Cat
+//Cat myCat = new Cat(); // erstellt eine Instanz der Klasse Cat und weißt sie der Variable mit dem Namen myCat zu
+//myCat.Color = "black";
+//Console.WriteLine("The color of my Cat is " + myCat.Color);
+
+Cat myCat = new Cat(new DateTime(2008, 01, 18));
+myCat.Color = "black";
+Console.WriteLine("The color of my Cat is " + myCat.Color);
+Console.WriteLine("The age of my Cat is " + myCat.Age + " years.");
+
