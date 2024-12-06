8825616a25f9ef1a2bd6bc8dda72646286946480
BurakSahin2060 <144993496+BurakSahin2060@users.noreply.github.com>
2024-12-06 11:01:51 +0100

    Revert "Geräusche und Beine | DONE"

--- 07_Objektorientierung/07_Objektorientierung/07_Objektorientierung/Animal.cs
+++ 07_Objektorientierung/07_Objektorientierung/07_Objektorientierung/Animal.cs
@@ -3,23 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace _07_Objektorientierung
 {
     class Animal : Lebewesen
-    abstract class Animal : Lebewesen
     {
-        public Animal(string name, DateTime birthDate, string color) : base(birthDate, color, name)
-        public int Beine { get; set; }
-        public string TierTyp { get; set; }
-        public Animal(string name, DateTime birthDate, string color, int beine, string tierTyp) : base(birthDate, color, name)
+        public string Name { get; set; }
+        public Animal(string name, DateTime birthDate) : base(birthDate)
+            public Animal(string name, DateTime birthDate, string color) : base(birthDate, color, name)
         {
 
-            TierTyp = tierTyp;
-            Beine = beine;
+            Name = name;
         }
-        public abstract void gibTypischenLautVonDir();
         public override string ToString()
         {
             return Name;
