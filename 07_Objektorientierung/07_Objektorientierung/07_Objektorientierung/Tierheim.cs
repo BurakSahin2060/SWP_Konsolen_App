3e8a3baf783ce6ba09eccf8ad04799b325ff37d9
BurakSahin2060 <144993496+BurakSahin2060@users.noreply.github.com>
2024-12-06 11:59:56 +0100

    Revert "Revert "Geräusche und Beine | DONE""

---
+++ 07_Objektorientierung/07_Objektorientierung/07_Objektorientierung/Tierheim.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Objektorientierung
+{
+    class Tierheim
+    {
+        public List<Cat> tiere = new List<Cat>();
+
+        public void AddAnimal(Cat cat)
+        public List<Animal> tiere = new List<Animal>();
+        public void AddAnimal(Animal animal)
+        {
+            tiere.Add(cat);
+            Console.WriteLine($"\nEs wurde die neue Katze {cat} hinzugefügt!\n");
+            tiere.Add(animal);
+            Console.WriteLine($"\nEs wurde neues {animal.TierTyp} {animal} hinzugefügt!\n");
+        }
+        public void ShowAnimals()
+        {
+            int index = 1;
+            foreach (var cat in tiere)
+            {
+                Console.WriteLine(cat);
+            }
+        }
+    }
+}
