648bff4b86e95e65a9dfe11a3c526d48c186221d
BurakSahin2060 <burak.sahin@student.htldornbirn.at>
2024-12-06 11:00:47 +0100

    Geräusche und Beine | DONE

--- 07_Objektorientierung/07_Objektorientierung/07_Objektorientierung/Cat.cs
+++ 07_Objektorientierung/07_Objektorientierung/07_Objektorientierung/Cat.cs
@@ -5,45 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace _07_Objektorientierung;
-
-class Cat : Animal
+namespace _07_Objektorientierung
 {
+    class Cat : Animal
+    {
 
-    public string Color { get; set; }
-    public int Age { get { return (DateTime.Now.Year - _BirthDate.Year); } }
-
-    public Cat(DateTime birthDate, string color = "Nicht Verfügbar", string name = "nicht Verfügbar") : base(name, birthDate)
 
         public Cat(DateTime birthDate, string color = "Nicht Verfügbar", string name = "nicht Verfügbar") : base(name, birthDate, color)
-    {
-        Color = color;
-    }
-    public override string ToString()
-    {
-        return $"Name: {Name}, Farbe: {Color}, Alter: {Age}";
-    }
-}
-
-class Tierheim
-{
-    private List<Cat> cats = new List<Cat>();
-
-    public void AddCat(Cat cat)
-    {
-        cats.Add(cat);
-    }
-
-    public int CatCount
-    {
-        get { return cats.Count; }
-    }
-
-    public void ShowAllCats()
-    {
-        foreach (var cat in cats)
+            public Cat(DateTime birthDate, string color = "Nicht Verfügbar", string name = "nicht Verfügbar") : base(name, birthDate, color, 4, "Katze")
         {
-            Console.WriteLine($"Name: {cat.Name}, Farbe: {cat.Color}, Alter: {cat.Age} Jahre");
+
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}, Farbe: {Color}, Alter: {Age}";
+            return $"Name: {Name}, Farbe: {Color}, Alter: {Age}, Beine: {Beine}";
+        }
+        public override void gibTypischenLautVonDir()
+        {
+            Console.WriteLine("Meowwwwwww!");
         }
     }
 }
---
+++ 07_Objektorientierung/07_Objektorientierung/07_Objektorientierung/Dog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _07_Objektorientierung
+{
+    class Dog : Animal
+    {
+        public Dog(DateTime birthDate, string color = "Nicht Verfügbar", string name = "nicht Verfügbar") : base(name, birthDate, color, 4, "Hund")
+        {
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}, Farbe: {Color}, Alter: {Age}, Beine: {Beine}";
+        }
+        public override void gibTypischenLautVonDir()
+        {
+            Console.WriteLine("Wuff!");
+        }
+    }
+}
